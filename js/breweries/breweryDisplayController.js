8c01267fe5fc5e5cc588355d8f1c4f15429cffae
JLB14 <jereleb14@gmail.com>
2015-03-23 23:02:02 +0100

    affichage des bieres dans le detail brasserie
    compte les bieres d'une brasserie et les affiche dans un tableau sur
    clique

--- js/breweries/breweryDisplayController.js
+++ js/breweries/breweryDisplayController.js
@@ -1,13 +1,38 @@
 module.exports=function($scope,config,$location,rest,save,$document,modalService) {
 
-    $scope.brewery = config.activeBrewery;
+    $scope.localData = {};
+    $scope.beerOn = false;
 
     if (angular.isUndefined(config.activeBrewery)) {
         $location.path("breweries/");
     }
     else{
+        $scope.brewery = config.activeBrewery;
+        var id = $scope.brewery.id;
+        console.log(id);
+
         if($scope.brewery.photo == null || $scope.brewery.photo == ""){
             $scope.brewery.photo = "img/breweries/default.jpg"
         }
+        rest.getAll($scope.localData, "beers/brewery/" + id);
     }
+
+    $scope.getBeerCount = function(){
+        if(typeof $scope.localData["beers/brewery/" + id] !== 'undefined' && $scope.localData["beers/brewery/" + id].length > 0){
+            return $scope.localData["beers/brewery/" + id].length;
+        }
+        else{
+            return 0;
+        }
+    };
+
+    $scope.showBeers = function(){
+        if($scope.beerOn == false && $scope.getBeerCount() > 0){
+            $scope.beerOn = true;
+            $scope.beers = $scope.localData["beers/brewery/" + id];
+        }
+        else{
+            $scope.beerOn = false;
+        }
+    };
 };
