f7379630f5ffc487f4144fe62423c347cf0df087
Durand <vlad.zdk@gmail.com>
2015-03-24 10:04:52 +0100

    link to brewery detail in beer detail

--- js/beers/beerDisplayController.js
+++ js/beers/beerDisplayController.js
@@ -1,25 +1,35 @@
 module.exports=function($scope,config,$location,rest,save,$document,modalService) {
 
-    $scope.beer = config.activeBeer;
     $scope.brewery;
     $scope.localData = {};
-    rest.getAll($scope.localData, "breweries");
+
 
     if (angular.isUndefined(config.activeBeer)) {
         $location.path("beers/");
     }
     else{
+        $scope.beer = config.activeBeer;
         if($scope.beer.photo == null || $scope.beer.photo == ""){
             $scope.beer.photo = "img/beers/default.jpg"
         }
+        rest.getAll($scope.localData, "breweries/" + $scope.beer.idBrewery);
     }
 
     $scope.getBrewery = function(){
-        for(var i = 0; i < $scope.localData.breweries.length; i++){
+        $scope.brewery = $scope.localData["breweries/" + $scope.beer.idBrewery];
+        /*for(var i = 0; i < $scope.localData.breweries.length; i++){
             if($scope.localData.breweries[i].id == $scope.beer.idBrewery){
-                $scope.brewery = $scope.localData.breweries[i].name;
+                $scope.brewery = $scope.localData.breweries[i];
             }
-        }
-        return $scope.brewery;
+        }*/
+        return $scope.brewery.name;
     };
+
+    $scope.showBrewery = function(){
+        if(angular.isDefined($scope.brewery)){
+            config.activeBrewery=angular.copy($scope.brewery);
+            config.activeBrewery.reference=$scope.brewery;
+        }
+        $location.path("breweries/detail");
+    }
 };
